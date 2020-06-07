cdb0e2aac5794dad8b25e674d42c045ad3b12172
uroojtariq <35533607+Uroojtariq02@users.noreply.github.com>
2020-06-07 16:49:44 +0500

    Chapter 5

---
+++ Chapter 5/question3.js
@@ -0,0 +1,36 @@
+// Question 3
+// a
+var num;
+
+// b
+document.write("Value after variable declaration is: " + num);
+
+// c
+num = 5;
+
+// d
+document.write("Initial Value: " + num);
+
+// e
+num++;
+
+// f
+document.write("Value after increment is: " + num);
+
+// g
+num += 7
+
+// h
+document.write("Value after addition is: " + num);
+
+// i
+num--;
+
+// j
+document.write("Value after decrement is: " + num);
+
+// k
+num = num % 3;
+
+// l
+document.write("The remainder is: " + num);
